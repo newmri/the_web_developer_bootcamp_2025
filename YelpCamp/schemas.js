b5efa55dccf0e37db92ea89ea88f6edbb9061322
newmri <newmri@naver.com>
2025-06-15 18:55:18 +0900

    YelpCamp 리뷰 추가

--- YelpCamp/schemas.js
+++ YelpCamp/schemas.js
@@ -1,6 +1,6 @@
 const Joi = require("joi");
 
-module.exports.campgroundSchema = Joi.object({
+const campgroundSchema = Joi.object({
   campground: Joi.object({
     title: Joi.string().required(),
     price: Joi.number().required().min(0),
@@ -9,3 +9,12 @@ module.exports.campgroundSchema = Joi.object({
     description: Joi.string().required(),
   }).required(),
 });
+
+const reviewSchema = Joi.object({
+  review: Joi.object({
+    rating: Joi.number().required().min(1).max(5),
+    body: Joi.string().required(),
+  }).required(),
+});
+
+module.exports = { campgroundSchema, reviewSchema };
