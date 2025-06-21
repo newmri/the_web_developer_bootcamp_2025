3c8cee054eba481fe2b16b52f93179fcddd66e15
newmri <newmri@naver.com>
2025-06-22 01:03:15 +0900

    YelpCamp 회원가입, 로그인 추가

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -6,9 +6,13 @@ const session = require("express-session");
 const flash = require("connect-flash");
 const ExpressError = require("./utils/ExpressError");
 const methodOverride = require("method-override");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user");
 
-const campgrounds = require("./routes/campgrounds");
-const reviews = require("./routes/reviews");
+const userRoutes = require("./routes/users");
+const campgroundRoutes = require("./routes/campgrounds");
+const reviewRoutes = require("./routes/reviews");
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost:27017/yelp-camp";
 
@@ -40,17 +44,27 @@ const sessionConfig = {
     maxAge: 1000 * 60 * 60 * 24 * 7, // 7일
   },
 };
+
 app.use(session(sessionConfig));
 app.use(flash());
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use((req, res, next) => {
+  res.locals.currentUser = req.user;
   res.locals.success = req.flash("success");
   res.locals.error = req.flash("error");
   next();
 });
 
-app.use("/campgrounds", campgrounds);
-app.use("/campgrounds/:id/reviews", reviews);
+app.use("/", userRoutes);
+app.use("/campgrounds", campgroundRoutes);
+app.use("/campgrounds/:id/reviews", reviewRoutes);
 
 app.get("/", (req, res) => {
   res.render("home");
@@ -62,7 +76,7 @@ app.all("/{*any}", (req, res, next) => {
 
 app.use((err, req, res, next) => {
   const { statusCode = 500 } = err;
-  if (!err.message) err.message = "Something went wrong";
+  if (!err.message) err.message = "Something went wrong!";
   res.status(statusCode).render("error", { err });
 });
 
